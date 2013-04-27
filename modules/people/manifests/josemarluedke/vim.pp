b8d739c2c7f869ed5cd0f0bb984f0e9049677b4c
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-27 00:28:15 -0300

    Add puppet for vim

---
+++ modules/people/manifests/josemarluedke/vim.pp
@@ -0,0 +1,13 @@
+class people::josemarluedke::vim {
+  $home     = "/Users/${::luser}"
+
+  if !file_exists("$home/.vim/install.sh") {
+    exec { "curl https://raw.github.com/${boxen::config::login}/vimfiles/master/install.sh -o - | sh":
+      cwd => "/Users/${luser}"
+    }
+
+    exec { "rake install":
+      cwd => "/Users/${luser}/.vim/"
+    }
+  }
+}
