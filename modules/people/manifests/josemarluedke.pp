ca6236b5d4caf8b028f5502c341d04b6ee967576
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-27 01:49:47 -0300

    Add puppet for gems

--- modules/people/manifests/josemarluedke.pp
+++ modules/people/manifests/josemarluedke.pp
@@ -1,4 +1,5 @@
 class people::josemarluedke {
-  include people::josemarluedke::dotfiles
-  include people::josemarluedke::vim
+  include people::josemarluedke::applications::dotfiles
+  include people::josemarluedke::applications::vim
+  include people::josemarluedke::applications::gems
 }
