14c7efa1eadf4306f95b39f107f184f3f941902a
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-27 22:57:38 -0300

    Add condition for dotfiles script

--- modules/people/manifests/josemarluedke/applications/dotfiles.pp
+++ modules/people/manifests/josemarluedke/applications/dotfiles.pp
@@ -3,49 +3,51 @@ class people::josemarluedke::applications::dotfiles {
 
   $dotfiles_dir = "${$home}/.dotfiles"
 
-  file { $dotfiles_dir:
-    ensure => "directory",
-    owner => $::luser,
-    group => "staff"
-  }
+  if !file_exists(dotfiles_dir) {
+    file { $dotfiles_dir:
+      ensure => "directory",
+      owner => $::luser,
+      group => "staff"
+    }
 
-  repository { $dotfiles_dir:
-    source => "${boxen::config::login}/dotfiles"
-  }
+    repository { $dotfiles_dir:
+      source => "${boxen::config::login}/dotfiles"
+    }
 
-  file { "${$home}/.gitconfig":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/gitconfig",
-    require => Repository[$dotfiles_dir]
-  }
+    file { "${$home}/.gitconfig":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/gitconfig",
+      require => Repository[$dotfiles_dir]
+    }
 
-  file { "${$home}/.gitignore":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/gitignore",
-    require => Repository[$dotfiles_dir]
-  }
+    file { "${$home}/.gitignore":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/gitignore",
+      require => Repository[$dotfiles_dir]
+    }
 
-  file { "${$home}/.bash_profile":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/bash_profile",
-    require => Repository[$dotfiles_dir]
-  }
+    file { "${$home}/.bash_profile":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/bash_profile",
+      require => Repository[$dotfiles_dir]
+    }
 
-  file { "${$home}/.inputrc":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/inputrc",
-    require => Repository[$dotfiles_dir]
-  }
+    file { "${$home}/.inputrc":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/inputrc",
+      require => Repository[$dotfiles_dir]
+    }
 
-  file { "${$home}/.irbrc":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/irbrc",
-    require => Repository[$dotfiles_dir]
-  }
+    file { "${$home}/.irbrc":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/irbrc",
+      require => Repository[$dotfiles_dir]
+    }
 
-  file { "${$home}/.pryrc":
-    ensure => "link",
-    target => "${$dotfiles_dir}/dotfiles/pryrc",
-    require => Repository[$dotfiles_dir]
+    file { "${$home}/.pryrc":
+      ensure => "link",
+      target => "${$dotfiles_dir}/dotfiles/pryrc",
+      require => Repository[$dotfiles_dir]
+    }
   }
 }
